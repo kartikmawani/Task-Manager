7a30f794b7e11be40896f08db82e6e873d0120a9
kartikmanwani <kartikmanwani2006@gmail.com>
2025-11-17 23:59:37 +0530

    Setting up the route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,12 @@
  
 import {Routes,Route} from 'react-router-dom'
 import Home from '../pages/Home.jsx'
-import Completed from '../pages/Completed.jsx'
+import Companytask from '../pages/Companytask.jsx'
 import Personaltask from '../pages/Personaltask.jsx'
 import Friendtask from '../pages/Friendtask.jsx'
  
 import './App.css'
+
 
 function App() {
   
@@ -14,7 +15,7 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/Company-tasks" element={<Completed/>}/>
+      <Route path="/Company-tasks" element={<Companytask/>}/>
       <Route path="/Personal-tasks" element={<Personaltask/>}/>
        <Route path="/Friend-tasks" element={<Friendtask/>}/>
     </Routes>
